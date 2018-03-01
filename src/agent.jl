4c592c6becbc006ec93d57f9eb6a0a3c4d15595d
Andrej Oskin <skoffer@arkoniak.com>
2018-03-01 07:05:52 +0300

    Some meat for mcts and agent

--- src/agent.jl
+++ src/agent.jl
@@ -1,6 +1,10 @@
 abstract type AbstractAgent end
 
 struct Agent <: AbstractAgent
+  root::Node
+  cpuct::???
+  mcts::MCTS
+  model::???
 end
 
 function replay!(agent::AbstractAgent, memory::Memory)
@@ -14,6 +18,69 @@ end
 
 function get_preds(agent, state)
 end
+
+function evaluate_leaf(agent::AbstractAgent, leaf, value, done, breadcrumbs)
+  if done == 0
+    value, probs, allowed_actions = agent.get_preds(leaf.state)
+    probs = probs[allowed_actions]
+
+    for (idx, action) in enumerate(allowed_actions)
+      new_state, _, _ = take_action(leaf.state, action)
+      if new_state.id ∈ agent.mcts.nodes
+        node = agent.mcts.nodes[new_state.id]
+      else
+        node = Node(new_state)
+        add_node(agent.mcts, node)
+      end
+
+      new_edge = Edge(leaf, node, probs[idx], action)
+      add_edge(agent.mcts, leaf, new_edge)
+      #= push!(leaf.edges, new_edge) =#
+    end
+  else
+    ## Log game value
+  end
+
+  return (value, breadcrumbs)
+end
+
+function get_av(agent::AbstractAgent, tau)
+  edges = agent.mcts.root.edges
+  ppi = zeros(Int, agent.action_size)
+  values = zeros(Float64, agent.action_size)
+
+  for edge in edges
+    ppi[edge.action] = edge.stats[:N]^(one(tau)/tau)
+    values[edge.action] = edge.stats[:Q]
+  end
+
+  ppi ./= sum(ppi)
+
+  return ppi, values
+end
+
+function choose_action(agent::AbstractAgent, ppi, values, tau)
+  ## Note: tau is unused?
+  if tau == 0
+    action = rand(find(ppi .== max(ppi)))
+  else
+    action = indmax(rand(Multinomial(1, ppi)))
+  end
+
+  value = values[action]
+
+  return action, value
+end
+
+predict(agent::AbstractAgent, input_to_model) = predict(agent.model, input_to_model)
+
+function buildMCTS(agent::AbstractAgent, state)
+  logg("building new mcts tree")
+  agent.root = Node(state)
+  agent.mcts(agent.root, agent.cpuct)
+end
+
+change_rootMCTS(agent::AbstractAgent, state) = (agent.mcts.root = agent.mcts.nodes[state.id])
 
 ###################
 
--- src/mcts.jl
+++ src/mcts.jl
@@ -1,3 +1,10 @@
+## Note: there are two possible implementations of tree structure
+## 1. Abstract types + cross references
+## 2. Concrete types with integer ids and vectors of edges/nodes
+## They should be tested for speed, I'll start with the second one.
+
+using Distributions
+
 struct Node
   state
   player_turn
@@ -5,17 +12,78 @@ struct Node
   edges
 end
 function Node(state)
-  Node(state, state.player_turn, state.id, Vector())
+  Node(state, state.player_turn, state.id, Vector{Int}())
 end
 
 is_leaf(node::Node) = isempty(node.edges)
+length(node::Node) = length(node.edges)
 
 ########################
 struct Edge
+  out_node::Node
+  stats::Dict{Symbol, Float64}
 end
 
 ########################
 struct MCTS
+  root::Node
+  nodes::Dict{Int, Node}
+  edges::Dict{Int, Edge}
 end
 
+add_node(mcts::MCTS, node::Node) = (mcts.nodes[node.id] = node)
+length(mcts::MCTS) = length(mcts.nodes)
 
+function move_to_leaf(mcts::MCTS)
+  breadcrumbs = Vector{Int}()
+  current_node = mcts.root
+
+  done = 0
+  value = 0
+
+  while !is_leaf(current_node)
+    ## TODO: Remove this hack
+    maxQU = -99999
+
+    if current_node == mcts.root
+      epsilon = Config.EPSILON
+      nu = rand(Dirichlet(length(current_node), Config.ALPHA))
+    else
+      epsilon = 0
+      nu = zeros(Float64, length(current_node))
+    end
+
+    nb = 0
+    for edge in current_node.edges
+      nb += edges[edge_id].stats[:N]
+    end
+
+    for (idx, edge_id) in enumerate(current_node.edges)
+      edge = edges[edge_id]
+      u = mcts.cpuct * ((one(epsilon) - epsilon) * edge.stats[:P] + epsilon * nu[idx]) * sqrt(nb)/(one(epsilon) + edge.stats[:N])
+      q = edge.stats[:Q]
+      if u + q > maxQU
+        maxQU = u + q
+        simulation_action = ???
+        simulation_edge = edge
+      end
+    end
+
+    ???? = take_action(current_node.state, simulation_action)
+    current_node = nodes[simulation_edge.out_node]
+    push!(breadcrumbs, simulation_edge)
+  end
+
+  return current_edge, value, done, breadcrumbs
+end
+
+function back_fill(mcts::MCTS, leaf, value, breadcrumbs)
+  current_player = leaf.state.player_turn
+  for edge in breadcrumbs
+    player_turn = edge.player_turn
+    direction = player_turn == current_player ? 1 : -1
+    edge.stats[:N] += 1
+    edge.stats[:W] += value*direction
+    edge.stats[:Q] = edge.stats[:W] / edge.stats[:N]
+  end
+end
